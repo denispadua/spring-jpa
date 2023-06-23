8dd9458a478df30cd066568e276bd2800d935377
denispadua <denisdepadua@outlook.com>
2023-06-23 00:19:01 -0300

    remove white space

--- src/main/java/com/ecommerce/ecommercejpa/category/CategoryRepository.java
+++ src/main/java/com/ecommerce/ecommercejpa/category/CategoryRepository.java
@@ -3,5 +3,4 @@ package com.ecommerce.ecommercejpa.category;
 import org.springframework.data.repository.ListCrudRepository;
 
 public interface CategoryRepository extends ListCrudRepository<CategoryModel, Long>{
-
 }
