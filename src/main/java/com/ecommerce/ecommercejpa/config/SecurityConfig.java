870b91333b61a17b39b12d1ea064e09fc2b6529f
denispadua <denisdepadua@outlook.com>
2023-07-16 12:56:51 -0300

    create spring security config

---
+++ src/main/java/com/ecommerce/ecommercejpa/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.ecommerce.ecommercejpa.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authz -> authz
+            .anyRequest()
+            .permitAll());
+        http.csrf(cors -> cors.disable());
+        return http.build();
+    }
+}
