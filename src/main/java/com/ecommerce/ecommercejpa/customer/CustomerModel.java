eec842d2a6599b149ec14c74844af24723872860
denispadua <denisdepadua@outlook.com>
2023-07-29 21:24:29 -0300

    change type of id and column name

--- src/main/java/com/ecommerce/ecommercejpa/customer/CustomerModel.java
+++ src/main/java/com/ecommerce/ecommercejpa/customer/CustomerModel.java
@@ -1,5 +1,6 @@
 package com.ecommerce.ecommercejpa.customer;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,16 +9,23 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "customer")
-public class CustomerModel {
 
+public class CustomerModel {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private String id;
+    private Long id;
     private String cpf;
+    @Column(name = "fullname")
     private String fullName;
     private String email;
     private String password;
     
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
     public String getCpf() {
         return cpf;
     }
