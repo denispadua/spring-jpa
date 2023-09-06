aa7c5664332b98e8dbc948fa4e3691f67a4e2935
denispadua <denisdepadua@outlook.com>
2023-09-06 13:26:15 -0300

    disable cors

--- src/main/java/com/ecommerce/ecommercejpa/config/SecurityConfig.java
+++ src/main/java/com/ecommerce/ecommercejpa/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 public class SecurityConfig {
@@ -19,6 +20,11 @@ public class SecurityConfig {
             .anyRequest()
             .permitAll());
         http.csrf(cors -> cors.disable());
+        http.cors(httpSecurityCorsConfigurer -> 
+        httpSecurityCorsConfigurer.configurationSource(request -> 
+           new CorsConfiguration().applyPermitDefaultValues()
+        )
+    );
         return http.build();
     }
 }
